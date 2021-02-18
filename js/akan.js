322eb4fe4318e9cbd15a2c382be43e7046992304
Enydin <nelsonwand@gmail.com>
2021-02-18 19:47:44 +0300

    push

--- js/akan.js
+++ js/akan.js
@@ -1,6 +1,6 @@
-document.getElementById('DAY').innerHTML=DAY
-var DAY="min=1, max=7"
-var MONTH="min=1, max=31"
+document.getElementById().innerHTML=DAY
+var DAY="min=1, max=31"
+var MONTH="min=1, max=12"
 var YEAR="min=00, max=99"
 var CENTURY="min=19, max=20"
 
