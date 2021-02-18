3692d2b5817895334c41c3b105c711568742c41d
Enydin <nelsonwand@gmail.com>
2021-02-18 19:40:34 +0300

    added button

--- js/akan.js
+++ js/akan.js
@@ -1,1 +1,9 @@
-var siku=["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+document.getElementById('DAY').innerHTML=DAY
+var DAY="min=1, max=7"
+var MONTH="min=1, max=31"
+var YEAR="min=00, max=99"
+var CENTURY="min=19, max=20"
+
+function newFunction() {
+    return ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+}
