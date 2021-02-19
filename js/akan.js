1fa35aa03d56f721cfca8e33fb14a894da2d0e0c
Enydin <nelsonwand@gmail.com>
2021-02-19 12:48:35 +0300

    parameters added

--- js/akan.js
+++
@@ -1,9 +0,0 @@
-document.getElementById().innerHTML=DAY
-var DAY="min=1, max=31"
-var MONTH="min=1, max=12"
-var YEAR="min=00, max=99"
-var CENTURY="min=19, max=20"
-
-function newFunction() {
-    return ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-}
