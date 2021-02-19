5c9cd0a1ed3701753a85cdee2814ec035c32c84f
Enydin <nelsonwand@gmail.com>
2021-02-19 16:19:47 +0300

    added JS script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 
 
+function getInput(){
+    century = parseInt(document.getElementById("century").value);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    date = parseInt(document.getElementById("date").value);
 
-{ ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 }
+
+function calculateDay(){
+    getInput();
+    day = (((century/4) -2*century-1) + ((5*year/4)) + ((26*(month+1)/10)) + date ) % 7
+    console.log(day);
+
+}
+function output(){
+    day = calculateDay();
+    checkGender();
+    console.log("The function runs")
+}
+
+
+
+let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
