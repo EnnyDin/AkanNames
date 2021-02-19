d1b5387d3b5d577e5f02cead8ea019d0f053017e
Enydin <nelsonwand@gmail.com>
2021-02-19 16:51:13 +0300

    made changes to html

--- js/script.js
+++ js/script.js
@@ -10,17 +10,11 @@ function getInput(){
 
 function calculateDay(){
     getInput();
-    day = (((century/4) -2*century-1) + ((5*year/4)) + ((26*(month+1)/10)) + date ) % 7
+    day= (((century/4)-2*century-1) + ((5*year/4)) + ((26*(month+1)/10)) + date ) % 7;
     console.log(day);
 
 }
-function output(){
-    day = calculateDay();
-    checkGender();
-    console.log("The function runs")
-}
 
 
 
-let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
