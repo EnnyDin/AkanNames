e89a1498f79a5ec2a0fffd8457db769cd8d9395c
EnnyDin <78684712+EnnyDin@users.noreply.github.com>
2021-02-22 19:41:40 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 
 
 function getInput(){
-    var century = (document.getElementById("century").value);
-    var year = (document.getElementById("year").value);
-    var month = (document.getElementById("month").value);
-    var date = (document.getElementById("date").value);
+    var century = parseInt(document.getElementById("century").value);
+    var year = parseInt(document.getElementById("year").value);
+    var month = parseInt(document.getElementById("month").value);
+    var date = parseInt(document.getElementById("date").value);
 
     
    if (century <15  || century >20){
@@ -25,13 +25,41 @@ function getInput(){
 
    }
 
+    
+function result(){
+    var display = calculateDay();
+    checkGender()
+}
 function calculateDay(){
     getInput();
-    day= (((century/4)-2*century-1) + ((5*year/4)) + ((26*(month+1)/10)) + date ) % 7;
-    console.log(day);
-    return (Math.floor(day));
+    day= ((((century/4)-2*century-1) + ((5*year/4)) + ((26*(month+1)/10)) + date ) % 7) -1;
    
+    return (Math.floor(day));
+    console.log(day);
+    if (day<0){
+        day = day * -1;
+        console.log(day)
     }
+    else if (day>0){
+        return day;
+    }
+    }
+
+    function checkGender(){
+     var male=document.getElementById("First").value
+     var female=document.getElementById("Second").value
+
+     if(male=="Male"){
+     console.log(maleNames[0])
+     }
+     else if (female=="Female"){
+     console.log(femaleNames)
+     }
+
+
+
+    }
+
     let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
     let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     let femaleNames = ["Akosua", "Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
